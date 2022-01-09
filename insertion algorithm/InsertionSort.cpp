0517d4d8c89d3adbc7b0dbdfbfa948c8b7049c03
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-09 14:43:20 +0530

    insertion sorting done in java, python and CPP

---
+++ insertion algorithm/InsertionSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {20, 15, 10, 5, 1};
+    int n = sizeof(arr) / sizeof(arr[2]); // sizeof function returns the total size of array in bytes stored in memory. so we take the sizeof of array and first item and then divide them to get the number of elements of the array
+    cout << n << " " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // sorting algorithm
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
--- insertion algorithm/InsertionSort.java
+++ insertion algorithm/InsertionSort.java
@@ -1,13 +1,19 @@
+// import java.io.ObjectInputFilter.Status;
+// import java.util.*;
+// import java.util.regex.Pattern;
+
 public class InsertionSort {
 
     public static int[] sort(int[] arr) {
         int n = arr.length;
+
         for (int i = 1; i < n; i++) {
             int j = i - 1;
-            int key = arr[j];
+            int key = arr[i];
+
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
-                printArray(arr);
+                // printArray(arr);
                 j = j - 1;
             }
             arr[j + 1] = key;
@@ -15,17 +21,19 @@ public class InsertionSort {
         return arr;
     }
 
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ' ');
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
         int[] sample_arr = { 20, 15, 10, 5, 1 };
-        int[] sorted_arr = sort(sample_arr);
         printArray(sample_arr);
-    }
 
+        int[] sorted_arr = sort(sample_arr);
+        printArray(sorted_arr);
+    }
 }
