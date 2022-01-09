ae60714411cb49bf8edbff3fe69f5670dcae8af1
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-09 13:17:23 +0530

    insertion sort in python

---
+++ InsertionSort.py
@@ -0,0 +1,20 @@
+def InsertionSort(arr):
+    n = len(arr)
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i - 1
+
+        while (j>=0 and arr[j] > key):
+            arr[j+1] = arr[j]
+            j = j-1
+        
+        arr[j+1] = key
+    
+    return arr
+
+sample_arr = [1, 20,15, 10, 9,1]
+
+sorted_arr = InsertionSort(sample_arr)
+
+print(sorted_arr)
